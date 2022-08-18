6e42083bfed405c1b0fd963609cc35fff68275d3
marsellosj <84620885+marsellosj@users.noreply.github.com>
2022-08-18 18:26:38 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "29.03.2003"; // Дата рождения
+var fullname = "Монько Сергій Андрійович" // ФИО
+var name = "Сергій" // Имя
+
+// Паспорт
+var passport_id = "004930351" // Номер паспорта
+
+// КПП
+var kpp_id = "2848367739" // ИНН
+
+// Права
+var rights_valid_until = "06.03.2023" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 7231" // Права "Видав
+var rights_id = "KBE928639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "11.04.2023" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
